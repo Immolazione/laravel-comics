926752f1d0a3d71b4708cd74cdbec693ba5ce3c1
Immolazione <federico.deered@gmail.com>
2022-03-16 16:25:22 +0100

    add old markup

---
+++ resources/views/home.blade.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div id="header-navbar">
+              <img src="" alt="Logo DC">
+              <nav>
+                <ul>
+                  <li><a href="">AA</a></li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+    </header>
+    <main>
+            <section id="jumbotron">
+            </section>
+            <section id="content">
+                <div class="container">
+                    <div id="current-series">CURRENT SERIES</div>
+                    <div class="card-box">
+                        <div class="card">
+                            <img src="" alt="">
+                            <div>AA</div>
+                        </div>
+                    </div>
+                    <div class="load-more">
+                        <button type="button">LOAD MORE</button>
+                    </div>
+                </div>
+            </section>
+            <section id="services-section">
+                <div class="container">
+                    <div class="services">
+                        <div class="service">
+                            <img src="" alt="">
+                            <h1>AA</h1>
+                        </div>
+                    </div>
+                </div>
+            </section>
+    </main>
+    <footer>
+        <section id="footer-links">
+            <div class="container">
+                <div class="footer-cols">
+                    <div class="footer-lists">
+                        <h2>AA</h2>
+                        <ul>
+                            <li>
+                                <a href="subtitle.url">AA</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <img src="../assets/img/dc-logo-bg.png" alt="DC Logo">
+                </div>
+            </div>
+        </section>
+        <section id="sign-and-follow">
+            <div class="container">
+                <div id="sign-and-follow-cols">
+                    <button type="button" class="sign-up">SIGN-UP NOW!</button>
+                    <div class="socials">
+                        <h2>FOLLOW US</h2>
+                        <img src="" alt="Follow">
+                        <img src="" alt="Follow">
+                        <img src="" alt="Follow">
+                        <img src="" alt="Follow">
+                        <img src="" alt="Follow">
+                    </div>
+                </div>
+            </div>
+        </section>
+    </footer>
+</body>
+</html>
