ea877be5c445458e63dc37e2c028d0e61c18ab3d
Immolazione <federico.deered@gmail.com>
2022-03-21 14:17:28 +0100

    add header and footer

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,69 @@
+@php
+$socials = config('footer')
+@endphp
+
+<footer>
+    <div class="footer-top">
+        <div class="container">
+            <div id="links">
+                <div id="group">
+                    <div class="list">
+                        <h3>DC COMICS</h3>
+                        <ul>
+                            @foreach($comics as $comic)
+                            <li>{{$comic}}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                    
+                    <div class="list">
+                        <h3>SHOP</h3>
+                        <ul>
+                            @foreach($shops as $shop)
+                            <li>{{$shop}}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+                <div class="list">
+                    <h3>DC</h3>
+                    <ul>
+                        @foreach($dcs as $dc)
+                        <li>{{$dc}}</li>
+                        @endforeach
+                    </ul>
+                </div>
+                <div class="list">
+                    <h3>SITES</h3>
+                    <ul>
+                        @foreach($sites as $site)
+                        <li>{{$site}}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+            <div class="logo">
+                <figure>
+                    <img src="{{asset("../images/dc-logo-bg.png")}}" alt="Logo dc" />
+                </figure>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="container">
+            <div id="button">
+                <button><a href="#">SIGN-UP NOW!</a></button>
+            </div>
+            <div id="social">
+                <h4>FOLLOW US</h4>
+                <ul>
+                    @foreach($socials as $social)
+                    <li>
+                        <img src="{{asset("images/" . $social['url'])}}" alt="{{$social['name']}}">
+                    </li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,28 @@
+@php
+$navs = config('navbar');
+@endphp
+
+<header>
+    <div id="banner">
+        <div class="container">
+            <div class="banner">DC POWER VISA</div>
+            <div class="banner">ADDITIONAL DC SITES</div>
+        </div>
+    </div>
+    <div class="container">
+        <div id="logo">
+            <figure>
+                <a href="#"><img src="{{asset('images/dc-logo.png')}}" /></a>
+            </figure>
+        </div>
+        <nav>
+            <ul>
+                @foreach($navs as $nav)
+                <li>
+                    <a href="{{ $nav['url'] }}">{{ $nav['name'] }}</a>
+                </li>
+                @endforeach
+            </ul>
+        </nav>
+    </div>
+</header>
